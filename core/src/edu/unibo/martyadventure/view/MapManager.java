3607f7683da510003acc449c14b06bd2e19b905e
damiano101 <damiano.morandi@studio.unibo.it>
2021-06-06 11:06:31 +0200

    refactor: map manager code split into functions

--- core/src/edu/unibo/martyadventure/view/MapManager.java
+++ core/src/edu/unibo/martyadventure/view/MapManager.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.objects.EllipseMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Ellipse;
 import com.badlogic.gdx.math.Vector2;
 
 public class MapManager {
@@ -18,11 +19,13 @@ public class MapManager {
     private Vector2 biffStartPosition;
 
 
-    //map names
-    public static enum Maps{
+    // map names
+    public static enum Maps {
         MAP1, MAP2, MAP3
     }
-    //map path
+
+
+    // map path
     private static final String MAP_1_PATH = "Level/Map/map1.tmx";
     private static final String MAP_2_PATH = "Level/Map/map2.tmx";
     private static final String MAP_3_PATH = "Level/Map/map3.tmx";
@@ -36,23 +39,54 @@ public class MapManager {
     private static final String MARTY_SPAWN_OBJECT_NAME = "MartySpawnObject";
     private static final String BIFF_SPAWN_OBJECT_NAME = "BiffSpawnObject";
 
-    
-    //unit scale
-    public final static float UNIT_SCALE = 1/16f;
-    
-    private Hashtable<Maps,String> mapTable;
-    
+    // unit scale
+    public final static float UNIT_SCALE = 1 / 16f;
+
+    private Hashtable<Maps, String> mapTable;
+
     private TiledMap currentMap;
     private Future<TiledMap> preLoadedMap;
     private Maps currentMapName;
     private Maps preLoadedMapName;
-    
-    //map layers
+
+    // map layers
     private MapLayer martySpawnLayer;
+    private MapLayer biffSpawnLayer;
     private MapLayer collisionLayer;
     private MapLayer pacManLayer;
     private MapLayer enemySpawnLayer;
-    private MapLayer biffSpawnLayer;
+
+
+    /**
+     * @return the map path for the given map name.
+     */
+    private String getMapPath(final Maps mapName) throws IOException {
+        final String path = mapTable.get(mapName);
+        if (path.isEmpty()) {
+            throw new IOException("Invalid map path");
+        }
+        return path;
+    }
+
+    /**
+     * @return the layer with the given name from the current map.
+     */
+    private MapLayer getLayer(final String layerName) throws IOException {
+        final MapLayer layer = currentMap.getLayers().get(layerName);
+        if (collisionLayer == null) {
+            throw new IOException("No '" + layerName + "' layer found.");
+        }
+        return layer;
+    }
+
+    /**
+     * @return the spawn point with the given name in the given layer.
+     */
+    private Vector2 getSpawnPoint(final MapLayer layer, final String objName) {
+        final EllipseMapObject obj = (EllipseMapObject) layer.getObjects().get(objName);
+        final Ellipse ellipse = obj.getEllipse();
+        return new Vector2(ellipse.x * UNIT_SCALE, ellipse.y * UNIT_SCALE);
+    }
 
     // initialize the hashtable
     public MapManager() {
@@ -71,35 +105,33 @@ public class MapManager {
     public Maps getCurrentMapName() {
         return currentMapName;
     }
-    
-    /** Start loading the given map from file into memory 
+
+    /**
+     * Start loading the given map from file into memory
+     * 
      * @param mapName the map name you want to start to load
-     * @throws IOException **/
+     * @throws IOException
+     **/
     public void preLoadMap(Maps mapName) throws IOException {
-      //get the map path from the table and check it
-        String mapPath = mapTable.get(mapName);
-        if (mapPath.isEmpty()) {
-            throw new IOException("Invalid map path");
-        }
+        final String mapPath = getMapPath(mapName);
         preLoadedMap = Toolbox.getMap(mapPath);
         preLoadedMapName = mapName;
     }
 
-    /** Load the given map name from file to the local memory 
+    /**
+     * Load the given map name from file to the local memory
+     *
      * @return the current loaded map name
      * @param mapName the map name you want to load
-     * @throws ExecutionException 
+     * @throws ExecutionException
      * @throws InterruptedException
-     * @throws IOException **/
+     * @throws IOException
+     **/
     public void loadMap(Maps mapName) throws InterruptedException, ExecutionException, IOException {
 
-        //get the map path from the table and check it
-        String mapPath = mapTable.get(mapName);
-        if (mapPath.isEmpty()) {
-            throw new IOException("Invalid map path");
-        }
-        
-        //if we are using another map we dispose that and free memory
+        final String mapPath = getMapPath(mapName);
+
+        // if we are using another map we dispose that and free memory
         if (currentMap != null) {
             currentMap.dispose();
         }
@@ -118,59 +150,21 @@ public class MapManager {
             preLoadedMapName = null;
         }*/
         currentMap = new TmxMapLoader().load(mapPath);
-        
+
         if (currentMap == null) {
             throw new IOException("Map not loaded, loading error");
-        } 
-
-        
-        //getting layers
-        collisionLayer = currentMap.getLayers().get(COLLISION_LAYER_NAME);
-        if (collisionLayer == null) {
-            throw new IOException("No collision layer loaded");
         }
 
-        martySpawnLayer = currentMap.getLayers().get(MARTY_SPAWN_LAYER_NAME);
-        if (martySpawnLayer == null) {
-            throw new IOException("No marty layer loaded");
-        }
+        // getting layers
+        this.collisionLayer = getLayer(COLLISION_LAYER_NAME);
+        this.martySpawnLayer = getLayer(MARTY_SPAWN_LAYER_NAME);
+        this.biffSpawnLayer = getLayer(BIFF_SPAWN_LAYER_NAME);
+        this.enemySpawnLayer = getLayer(ENEMY_SPAWN_LAYER_NAME);
+        this.pacManLayer = getLayer(PACMAN_LAYER_NAME);
 
-        biffSpawnLayer = currentMap.getLayers().get(BIFF_SPAWN_LAYER_NAME);
-        if (biffSpawnLayer == null) {
-            throw new IOException("No biff layer loaded");
-        }
-
-        enemySpawnLayer = currentMap.getLayers().get(ENEMY_SPAWN_LAYER_NAME);
-        if (enemySpawnLayer == null) {
-            throw new IOException("No enemy layer loaded");
-        }
-
-        pacManLayer = currentMap.getLayers().get(PACMAN_LAYER_NAME);
-        if (pacManLayer == null) {
-            throw new IOException("No pacman layer loaded");
-        }
-
-        // Setting Marty's spawn point
-        EllipseMapObject obj = (EllipseMapObject) martySpawnLayer.getObjects().get(MARTY_SPAWN_OBJECT_NAME);
-        if (playerStartPosition == null) {
-            playerStartPosition = new Vector2();
-        }
-
-        playerStartPosition.x = obj.getEllipse().x;
-        playerStartPosition.y = obj.getEllipse().y;
-        playerStartPosition.x = playerStartPosition.x * UNIT_SCALE;
-        playerStartPosition.y = playerStartPosition.y * UNIT_SCALE;
-        
-     // Setting biff spawn point
-        obj = (EllipseMapObject) biffSpawnLayer.getObjects().get(BIFF_SPAWN_OBJECT_NAME);
-        if (biffStartPosition == null) {
-            biffStartPosition = new Vector2();
-        }
-
-        biffStartPosition.x = obj.getEllipse().x -20f;
-        biffStartPosition.y = obj.getEllipse().y;
-        biffStartPosition.x = biffStartPosition.x * UNIT_SCALE;
-        biffStartPosition.y = biffStartPosition.y * UNIT_SCALE;
+        // Setting spawn points.
+        this.playerStartPosition = getSpawnPoint(this.martySpawnLayer, MARTY_SPAWN_OBJECT_NAME);
+        this.biffStartPosition = getSpawnPoint(this.biffSpawnLayer, BIFF_SPAWN_OBJECT_NAME);
     }
 
     /** @return the current loaded collision layer **/
@@ -182,7 +176,7 @@ public class MapManager {
     public Vector2 getPlayerStartPosition() {
         return playerStartPosition;
     }
-    
+
     /** @return biff start position of the current map **/
     public Vector2 getBiffStartPosition() {
         return biffStartPosition;
