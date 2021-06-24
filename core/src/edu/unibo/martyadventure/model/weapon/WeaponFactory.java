79e2cacbc53fc8efe038d3a6a102b572df4b3647
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-06-24 11:55:33 +0200

    refactor randomDamageMultiplier visibility

--- core/src/edu/unibo/martyadventure/model/weapon/WeaponFactory.java
+++ core/src/edu/unibo/martyadventure/model/weapon/WeaponFactory.java
@@ -128,7 +128,7 @@ public class WeaponFactory {
      * 
      * @return Random double between MIN and MAX
      */
-    public static float randomDamageMultiplier() {
+    private static float randomDamageMultiplier() {
         return ((ThreadLocalRandom.current().nextFloat() % MAX_DAMAGE_MULTIPLIER) + MIN_DAMAGE_MULTIPLIER);
     }
 
